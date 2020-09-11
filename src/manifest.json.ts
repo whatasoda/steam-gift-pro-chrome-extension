f5d81f666d40bd4191e950baf564a4032a85a2fa
whatasoda <git@whatasoda.me>
2020-09-12 02:26:55 +0900

    feat: add key

--- src/manifest.json.ts
+++ src/manifest.json.ts
@@ -6,8 +6,20 @@ const JSONFile = <T>(cacheable: boolean, gen: () => Promise<T>) => async () => (
   code: JSON.stringify(await gen()),
 });
 
+const key = [
+  // -----BEGIN PUBLIC KEY-----
+  'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArAaRsTkyGgAicD8Z3nh2',
+  'oX0kzAwNpO21vnGQfrcmbbFGvRvHxuEU5F2Su8/ar4FvdTkPxmesomSquyCTr10B',
+  'tX19M4IJ58LKWA/V9/kSYBKLS1zXy7RFjAmWRavepQz1kH2soTGW+QXsRMIum4ku',
+  'MgeAIkDFtveg4u+Iemrz97/5bhx7SkuxLJLIJqqHI5fAJjhfscg5/YbHj4h3QgBC',
+  'shDdMdUDdTfWkKdP4oVRaKgS8/TJuh5fzFZs/0eh45LdGVB+GsVHu29fEQ4zf8rr',
+  'ynWoUkY3v/6VxwMyebr2six0bE9hY3CKmowPB1KghRuPGO9GChtxQbiEPgMNcRMk',
+  'gQIDAQAB',
+  // -----END PUBLIC KEY-----
+];
+
 export = JSONFile<Manifest>(true, async () => ({
-  // key: '',
+  key: key.join(''),
   manifest_version: 2,
   version,
   name: 'Steam Gift Pro',
