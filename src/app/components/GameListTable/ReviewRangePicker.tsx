d0cec15940e05851d446cbc936fb3a25844ca96d
whatasoda <git@whatasoda.me>
2020-11-12 01:09:08 +0900

    chore: update reviewRangeFilter button icon

--- src/app/components/GameListTable/ReviewRangePicker.tsx
+++ src/app/components/GameListTable/ReviewRangePicker.tsx
@@ -7,17 +7,21 @@ import type { ComponentProps } from './container';
 interface ReviewRangePickerProps extends Pick<ComponentProps, 'table' | 'indexes' | 'info'> {}
 
 export const ReviewRangePicker = ({ table: { columns }, indexes, info: { minmax } }: ReviewRangePickerProps) => {
+  const up = columns[indexes.up];
+  const comp = columns[indexes.comp];
+  const down = columns[indexes.down];
+  const hasFilter = Boolean(up.filterValue || comp.filterValue || down.filterValue);
   return (
     <Popover
       position="right"
       content={
         <Wrapper>
-          <BetweenFilterPicker icon="thumbs-up" column={columns[indexes.up]} minmax={minmax.up} />
-          <BetweenFilterPicker icon="flow-review" column={columns[indexes.comp]} minmax={minmax.comp} />
-          <BetweenFilterPicker icon="thumbs-down" column={columns[indexes.down]} minmax={minmax.down} />
+          <BetweenFilterPicker icon="thumbs-up" column={up} minmax={minmax.up} />
+          <BetweenFilterPicker icon="flow-review" column={comp} minmax={minmax.comp} />
+          <BetweenFilterPicker icon="thumbs-down" column={down} minmax={minmax.down} />
         </Wrapper>
       }
-      children={<Button icon="filter" text="レビューで絞り込み" />}
+      children={<Button icon={hasFilter ? 'filter-keep' : 'filter'} text="レビューで絞り込み" />}
     />
   );
 };
